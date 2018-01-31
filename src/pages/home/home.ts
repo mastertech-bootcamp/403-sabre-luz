8d784f343e642aef335795fe7ce03d90aa0ac2a2
Bruno Pimentel <brunoapimentel@gmail.com>
2018-01-31 11:52:08 -0200

    Finaliza o sabre

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,22 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  sabre = 'assets/imgs/light-off.png';
+  som = new Audio('assets/sabreon.wav');
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ligarSabre(){
+    this.som.play();
+    
+    if(this.sabre == 'assets/imgs/light-on.png'){
+      this.sabre = 'assets/imgs/light-off.png';
+    }else{
+      this.sabre = 'assets/imgs/light-on.png';
+    }
+    
   }
 
 }
